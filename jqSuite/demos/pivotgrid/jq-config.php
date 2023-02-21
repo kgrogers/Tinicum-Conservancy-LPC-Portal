1171dba1ff40411f6c7f74412f60ff889e6e04b1
Ken Rogers <Ken Rogers@iname.com>
2023-02-21 01:44:18 +0000

    Modified paths in file to work on this server.

--- jqSuite/demos/pivotgrid/jq-config.php
+++ jqSuite/demos/pivotgrid/jq-config.php
@@ -3,8 +3,8 @@
 //define('DB_NAME', 'northwind');    // The name of the database
 //define('DB_HOST', 'localhost');    // 99% chance you won't need to change this value
 define('DB_DSN','mysql:host=localhost;dbname=northwind');
-define('DB_USER', 'root');     // Your MySQL username
-define('DB_PASSWORD', ''); // ...and password
+define('DB_USER', 'otto');     // Your MySQL username
+define('DB_PASSWORD', 'aDfp4C2EsRpFt$s#zr'); // ...and password
 define('DB_DATABASE', 'northwind'); // ...and password
 
 define('ABSPATH', '../../../');
